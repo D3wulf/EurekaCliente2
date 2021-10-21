d3a5d631f1e60340ad73fc18a8ab377cf99ebdcb
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-21 14:44:05 +0200

    añado endpoint

---
+++ src/main/java/cliente/eureka/ClienteEureka2/controlador/ClienteEndpoint.java
@@ -0,0 +1,31 @@
+package cliente.eureka.ClienteEureka2.controlador;
+
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RestControllerEndpoint(id="endPointCustomizado")
+public class ClienteEndpoint {
+	
+	@GetMapping("/")
+	public ResponseEntity<String> endPointCustomizado(){
+		
+		return new ResponseEntity<>("Hola desde el endpoint customizado", HttpStatus.OK);
+		
+	}
+	
+	@ResponseBody
+	@GetMapping("/persona/{nombre}")
+	public String prueba(@PathVariable String nombre) {
+		
+		String respuesta = "Hola: " + nombre;
+		return respuesta;
+		
+	}
+
+}
---
+++ src/main/java/cliente/eureka/ClienteEureka2/controlador/clienteController.java
@@ -0,0 +1,32 @@
+package cliente.eureka.ClienteEureka2.controlador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+@RestController
+public class clienteController {
+
+
+	
+	
+	
+	
+		
+		private Counter counter;
+		
+		//Utilidad de prometheus MeterRegistry
+		public clienteController(MeterRegistry registry) {
+			
+			this.counter = Counter.builder("saludos.hola").description("saludos totales").register(registry);
+			
+		}
+		// Aumenta el contador cuando entremos en http://localhost:8080/holamundo
+		@GetMapping(path="/saludos")
+		public String holaMundo() {
+			counter.increment();
+			return "Hola mundo";
+		
+		}
+}
